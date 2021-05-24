236e972306726a8b8dfb08cba1aabea24b43ce91
Clustern <homerun372@gmail.com>
2021-05-24 14:18:13 +0900

    사이드 메뉴 버튼 터치 시 사이드 메뉴 활성화 기능 작성

--- app/src/main/java/com/cookandroid/wifi_based_todolist/MainActivity.java
+++ app/src/main/java/com/cookandroid/wifi_based_todolist/MainActivity.java
@@ -1,14 +1,35 @@
 package com.cookandroid.wifi_based_todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView sideBarButton;
+    DrawerLayout sideBarDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ImageView 등록
+        sideBarButton = (ImageView) findViewById(R.id.sideBarButton);
+        //DrawerLayout 등록
+        sideBarDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        //사이드 메뉴 버튼 터치 시 사이드 바 활성화 기능
+        sideBarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sideBarDrawer.openDrawer(Gravity.RIGHT);
+            }
+        });
     }
 }
