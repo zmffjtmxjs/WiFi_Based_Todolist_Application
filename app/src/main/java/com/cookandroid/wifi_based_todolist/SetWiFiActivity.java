8745d52a5463c7986e2b6ad9f711fc4e8c74b10a
Clustern <homerun372@gmail.com>
2021-05-26 05:07:48 +0900

    WiFi 설정 화면 엑티비티의 기본 구성 작성

--- app/src/main/java/com/cookandroid/wifi_based_todolist/SetWiFiActivity.java
+++ app/src/main/java/com/cookandroid/wifi_based_todolist/SetWiFiActivity.java
@@ -1,4 +1,35 @@
 package com.cookandroid.wifi_based_todolist;
 
-public class SetWiFiActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SetWiFiActivity extends Activity {
+
+    ImageView cancelAddToDo, saveAddToDo;
+    TextView titleText;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_add_todo);
+
+        //ImageView 등록
+        cancelAddToDo = (ImageView) findViewById(R.id.cancelAddToDo);
+        //TextView 등록
+        titleText = (TextView) findViewById(R.id.titleText);
+
+        //화면 제목 표시
+        titleText.setText("할 일 편집");
+
+        //메인화면으로 돌아감
+        cancelAddToDo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 }
