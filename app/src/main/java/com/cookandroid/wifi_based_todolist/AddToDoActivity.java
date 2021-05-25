033631cb3561f073ccfa34fa22847176e5629007
Clustern <homerun372@gmail.com>
2021-05-26 02:49:18 +0900

    할일 추가 화면 표시를 위한 코드 작성

--- app/src/main/java/com/cookandroid/wifi_based_todolist/AddToDoActivity.java
+++ app/src/main/java/com/cookandroid/wifi_based_todolist/AddToDoActivity.java
@@ -1,4 +1,13 @@
 package com.cookandroid.wifi_based_todolist;
 
-public class AddToDoActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddToDoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_add_todo);
+    }
 }
