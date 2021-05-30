af169fb36ad8ea393e80d90fa6653bedb98e16fd
Clustern <homerun372@gmail.com>
2021-05-30 20:42:52 +0900

    만료 날짜 및 시간 선택 기능 구현

--- app/src/main/java/com/cookandroid/wifi_based_todolist/page/AddToDoActivity.java
+++ app/src/main/java/com/cookandroid/wifi_based_todolist/page/AddToDoActivity.java
@@ -1,29 +1,31 @@
 package com.cookandroid.wifi_based_todolist.page;
 
 import android.app.Activity;
-import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cookandroid.wifi_based_todolist.R;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 public class AddToDoActivity extends Activity {
 
     ImageView cancelAddToDo, saveAddToDo;
-    TextView titleText, pickDue;
+    TextView titleText, pickDueDate;
+
     Calendar cal = Calendar.getInstance();
-    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd  (EEE) aaa HH시mm분",
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd  '('EEE')' aaa h'시'mm'분'",
             new Locale(Locale.KOREAN.getLanguage(), Locale.KOREAN.getCountry()));
     String date = dateFormat.format(cal.getTime());
+    String newPickDate;
 
     @Override
     protected void onCreate(Bundle saveInstanceState) {
@@ -35,10 +37,9 @@ public class AddToDoActivity extends Activity {
         cancelAddToDo = (ImageView) findViewById(R.id.discard);
         //TextView 등록
         titleText = (TextView) findViewById(R.id.titleText);
-        pickDue = (TextView) findViewById(R.id.pickedDue);
+        pickDueDate = (TextView) findViewById(R.id.pickedDue);
 
-        pickDue.setText(date);
-
+        pickDueDate.setText(date);
 
         //메인화면으로 돌아감
         cancelAddToDo.setOnClickListener(new View.OnClickListener() {
@@ -56,11 +57,22 @@ public class AddToDoActivity extends Activity {
         startActivityForResult(intent, 1);
     }
 
+    //팝업 창이 종료된 이후 확인을 눌렀을 경우
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == 1) {
-            if(requestCode == RESULT_OK) {
+            if(resultCode == RESULT_OK) {
+                newPickDate = data.getStringExtra("NewDueDate");
+                SimpleDateFormat transport = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 
+                //가져온 시간을 Calender타입으로 변환
+                try {
+                    cal.setTime(transport.parse(newPickDate));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                pickDueDate.setText(dateFormat.format(cal.getTime()));
             }
         }
     }
