a398eaa47275a0a45bab5cf159ff3bb10b58cb4f
Young37 <20171470@g.dongseo.ac.kr>
2021-06-03 18:25:47 +0900

    "연결된 WiFI 선택" 버튼 누르면 Mac주소  가져와서 Toast로 짧게 띄우고 textView에 표시합니다.

--- app/src/main/java/com/cookandroid/wifi_based_todolist/page/SetWiFiActivity.java
+++ app/src/main/java/com/cookandroid/wifi_based_todolist/page/SetWiFiActivity.java
@@ -3,20 +3,32 @@ package com.cookandroid.wifi_based_todolist.page;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cookandroid.wifi_based_todolist.R;
+import com.cookandroid.wifi_based_todolist.module.MacAddress;
 
 public class SetWiFiActivity extends Activity {
 
+    //............toolbar 관련 요소
     ImageView cancelAddToDo, saveAddToDo;
     TextView titleText;
+
+    //............toolbar 외의 요소
+    Button selectWifi; // 현재 연결 된 wifi를 선택하는 버튼
+    Button manageLocation; // 이전에 설정한 장소를 관리하는 (팝업?화면? 뭐였었죠)창을 띄우는 버튼
+    Button locationName; // 선택한 wifi에 대응되는 위치 이름을 입력하기 위한 버튼
+    TextView selectedMac; // 선택 된 MAC주소를 보여줍니다.
 
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_set_wifi);
+
+        //.................toolbar 관련 코드
 
         //ImageView 등록
         cancelAddToDo = (ImageView) findViewById(R.id.discard);
@@ -33,5 +45,23 @@ public class SetWiFiActivity extends Activity {
                 finish();
             }
         });
+
+        //..................toolbar 이외의 기능 코드
+
+        selectWifi = (Button) findViewById(R.id.selectWifi);
+        manageLocation = (Button) findViewById(R.id.manageLocation);
+        locationName = (Button) findViewById(R.id.locationName);
+        selectedMac = (TextView) findViewById(R.id.selectedMac);
+
+        selectWifi.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String macAddress = MacAddress.getMACAddress("wlan0"); // 현재 연결 된 wifi의 Mac주소를 가져 옵니다
+                Toast.makeText(getApplicationContext(), macAddress + "선택 됨", Toast.LENGTH_SHORT).show(); // Mac주소를 Toast로 띄워줍니다
+                selectedMac.setText("현재 선택 : " + macAddress); // 선택 된 Mac주소를 보여줍니다.
+            }
+        });
+        
+        
+
     }
 }
