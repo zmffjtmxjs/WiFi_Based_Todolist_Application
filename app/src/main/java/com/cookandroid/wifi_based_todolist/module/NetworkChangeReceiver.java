048180763000a102ae6d74e58a7969aa95a9050f
Young37 <20171470@g.dongseo.ac.kr>
2021-06-09 13:14:25 +0900

    네트워크 변경 감지 리시버, 백그라운드 서비스 추가

---
+++ app/src/main/java/com/cookandroid/wifi_based_todolist/module/NetworkChangeReceiver.java
@@ -0,0 +1,15 @@
+package com.cookandroid.wifi_based_todolist.module;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NetworkChangeReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
--- app/src/main/java/com/cookandroid/wifi_based_todolist/page/MainActivity.java
+++ app/src/main/java/com/cookandroid/wifi_based_todolist/page/MainActivity.java
@@ -15,6 +15,7 @@ import com.cookandroid.wifi_based_todolist.DB.DAO.WifiDB;
 import com.cookandroid.wifi_based_todolist.DB.DTO.Todo;
 import com.cookandroid.wifi_based_todolist.DB.DTO.Wifi;
 import com.cookandroid.wifi_based_todolist.R;
+import com.cookandroid.wifi_based_todolist.module.BackgroundService;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.lang.reflect.Array;
@@ -44,6 +45,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //백그라운드 서비스 계속 작동시키기
+        Intent intent = new Intent(
+                getApplicationContext(),
+                BackgroundService.class);
+        startService(intent);// 백그라운드 서비스 "BackgroundService"를 시작합니다. 일단 어플이 시작되면 멈추지 않습니다.onStartCommand()가 실행됩니다.
 
         //DB부분 추가
         todoDB = new TodoDB(this);
