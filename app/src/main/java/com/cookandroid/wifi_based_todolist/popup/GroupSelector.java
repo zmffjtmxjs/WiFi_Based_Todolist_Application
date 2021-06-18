77d4591ade32f8ee5c52945387f70efb1d3ee774
Young37 <20171470@g.dongseo.ac.kr>
2021-06-18 13:18:36 +0900

    locationName 텍스트 값 못 읽어오던 문제 수정

--- app/src/main/java/com/cookandroid/wifi_based_todolist/popup/GroupSelector.java
+++ app/src/main/java/com/cookandroid/wifi_based_todolist/popup/GroupSelector.java
@@ -85,8 +85,6 @@ public class GroupSelector extends Activity {
         wifiList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), String.valueOf(preWifi.get(position)), Toast.LENGTH_SHORT).show();
-
                 //리스트 뷰의 위치를 preWifi 배열의 인덱스 값으로 사용하여 정보 추출 (DB에 맞게 변경 필요 시 자유롭게 바꿀 것)
                 Intent intent = new Intent();
                 intent.putExtra("locate",  String.valueOf(preWifi.get(position)));
